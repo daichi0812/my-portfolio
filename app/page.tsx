08fbc29f7ec3d4138e975c67a3e963a3e35f4e9e
Glound <glound.app@gmail.com>
2025-06-24 23:22:48 +0900

    変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Github, Linkedin, Twitter, Code, Smartphone, Cpu, ExternalLink, Menu, X } from 'lucide-react';
 
 // Main App Component
